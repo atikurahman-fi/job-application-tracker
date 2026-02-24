2178bf08d31206637e7ca53cc212023a470ef11a
atikurahman-fi <atikurahman.fi@gmail.com>
2026-02-24 11:30:38 +0200

    commit

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const mainContainer  = document.querySelector('main');
 const allFilterBtn       = document.getElementById('all-filter-btn');
 const interviewFilterBtn = document.getElementById('interview-filter-btn');
 const rejectedFilterBtn  = document.getElementById('rejected-filter-btn');
-// track active tab
+// track active tabbbbbbbbbbbbbbbbbbbbbbb
 let activeTab = 'all';
 
 // ─── CALCULATE & UPDATE COUNTS ─
